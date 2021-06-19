14d4f1117104f6515fa3c1d8eb9894f13d753793
Silvio Silva <smsilva@gmail.com>
2021-06-19 10:27:32 -0300

    Tutorial Terragrunt

--- terragrunt/frontend-app/main.tf
+++ terragrunt/frontend-app/main.tf
@@ -5,12 +5,19 @@ terraform {
       version = "=2.63.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name   = "terraform"
+    storage_account_name  = "silviosterraform"
+    container_name        = "state"
+    key                   = "frontend-app.json"
+  }
 }
 
 provider "azurerm" {
-  features {}
   tenant_id       = "a267367d-d04d-4a6b-84ef-0cc227ed6e9f"
   subscription_id = "ddc30188-075a-470d-a6ca-05a1987c51a3"
+  features {}
 }
 
 locals {
