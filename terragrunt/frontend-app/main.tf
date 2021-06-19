17f3054c5ed350bd28782c1ede12ef4c657bfc8b
Silvio Silva <smsilva@gmail.com>
2021-06-19 09:33:55 -0300

    Seguindo Tutorial Inicial do Terragrunt

---
+++ terragrunt/frontend-app/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.63.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  tenant_id       = "a267367d-d04d-4a6b-84ef-0cc227ed6e9f"
+  subscription_id = "ddc30188-075a-470d-a6ca-05a1987c51a3"
+}
+
+locals {
+    common = yamldecode(file("./variables/default.yaml"))
+}
+
+resource "azurerm_resource_group" "default" {
+    name     = "${local.common.resource_group.name}"
+    location = "${local.common.location}"
+    tags     = {}
+}
