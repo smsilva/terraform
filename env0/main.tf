6bd4af713e33b1fb05cd5755349b53ef66ef139a
Silvio Silva <smsilva@gmail.com>
2021-06-21 20:53:57 -0300

    Issue on Apply

--- env0/main.tf
+++ env0/main.tf
@@ -14,3 +14,15 @@ resource "env0_project" "example" {
   name        = "example"
   description = "Example project"
 }
+
+resource "env0_template" "example" {
+  name        = "mytemplate"
+  description = "Example template"
+  repository  = "https://github.com/env0/templates"
+  path        = "aws/hello-world"
+}
+
+resource "env0_template_project_assignment" "assignment" {
+  template_id = env0_template.example.id
+  project_id  = env0_project.example.id
+}
