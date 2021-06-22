4b2365b2c1602ab2656a50cc037c7f57f4cb37b6
Silvio Silva <smsilva@gmail.com>
2021-06-22 17:28:17 -0300

    for_each

---
+++ for_each/main.tf
@@ -0,0 +1,15 @@
+module "prefix" {
+  source = "../modules/test"
+
+  for_each = {
+    a_group        = "eastus"
+    another_group  = "westus2"
+    more_one_group = "centralus"
+  }
+  
+  name = "${each.key}_${each.value}"
+}
+
+output "newname" {
+  value = module.prefix
+}
