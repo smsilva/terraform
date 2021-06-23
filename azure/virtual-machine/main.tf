786d0099582f4ac699f89916f5f3e3c5a8d8cb8e
Silvio Silva <smsilva@gmail.com>
2021-06-23 08:29:09 -0300

    Update OS Image

--- azure/virtual-machine/main.tf
+++ azure/virtual-machine/main.tf
@@ -45,7 +45,7 @@ resource "azurerm_virtual_machine" "main" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
