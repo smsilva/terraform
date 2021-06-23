6b0108125a9d885755d8c09ed3ea8027ed11497e
Silvio Silva <smsilva@gmail.com>
2021-06-23 08:40:25 -0300

    Teste

--- azure/virtual-machine/main.tf
+++ azure/virtual-machine/main.tf
@@ -51,20 +51,14 @@ resource "azurerm_virtual_machine" "main" {
   }
 
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "myosdisk-${count.index}"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-
   os_profile_linux_config {
-    disable_password_authentication = false
+    disable_password_authentication = true
   }
 
 }
