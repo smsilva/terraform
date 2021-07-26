3d25e35401dbcd4d27c03110483ec7d8ebecd78b
Silvio Silva <smsilva@gmail.com>
2021-07-26 11:47:09 -0300

    Network, Subnet, Router novos criados

--- openstack/main.tf
+++ openstack/main.tf
@@ -41,11 +41,59 @@ resource "openstack_compute_instance_v2" "server_1" {
   }
 }
 
+resource "openstack_compute_instance_v2" "server_2" {
+  name            = "server-02"
+  image_id        = data.openstack_images_image_v2.cirros.id
+  flavor_id       = data.openstack_compute_flavor_v2.m1_tinny.flavor_id
+  key_pair        = openstack_compute_keypair_v2.silvios.name
+  security_groups = ["default"]
+
+  metadata = {
+    this = "that"
+  }
+
+  network {
+    name = openstack_networking_network_v2.network_1.name
+  }
+}
+
 resource "openstack_compute_floatingip_v2" "floatingip_1" {
+  pool = data.openstack_networking_network_v2.public.name
+}
+
+resource "openstack_compute_floatingip_v2" "floatingip_2" {
   pool = data.openstack_networking_network_v2.public.name
 }
 
 resource "openstack_compute_floatingip_associate_v2" "floatingip_1_server_1" {
   floating_ip = openstack_compute_floatingip_v2.floatingip_1.address
   instance_id = openstack_compute_instance_v2.server_1.id
+}
+
+resource "openstack_compute_floatingip_associate_v2" "floatingip_2_server_2" {
+  floating_ip = openstack_compute_floatingip_v2.floatingip_2.address
+  instance_id = openstack_compute_instance_v2.server_2.id
+}
+
+resource "openstack_networking_network_v2" "network_1" {
+  name           = "network_1"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "subnet_1" {
+  name       = "subnet_1"
+  network_id = openstack_networking_network_v2.network_1.id
+  cidr       = "192.168.223.0/24"
+  ip_version = 4
+}
+
+resource "openstack_networking_router_v2" "router_1" {
+  name                = "my_router"
+  admin_state_up      = true
+  external_network_id = data.openstack_networking_network_v2.public.id
+}
+
+resource "openstack_networking_router_interface_v2" "router_1_interface_1" {
+  router_id = openstack_networking_router_v2.router_1.id
+  subnet_id = openstack_networking_subnet_v2.subnet_1.id
 }
