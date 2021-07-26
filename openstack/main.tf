a44f30b82585ef364c7c9da91039e796e97752d8
Silvio Silva <smsilva@gmail.com>
2021-07-26 11:16:00 -0300

    Floating IP Associado

--- openstack/main.tf
+++ openstack/main.tf
@@ -20,10 +20,6 @@ data "openstack_networking_network_v2" "public" {
   name = "external"
 }
 
-resource "openstack_compute_floatingip_v2" "floatingip_1" {
-  pool = data.openstack_networking_network_v2.public.name
-}
-
 resource "openstack_compute_keypair_v2" "silvios" {
   name       = "silvios"
   public_key = file("/home/silvios/.ssh/id_rsa.pub")
@@ -45,7 +41,11 @@ resource "openstack_compute_instance_v2" "server_1" {
   }
 }
 
-output "server_1" {
-  value = openstack_compute_instance_v2.server_1
-  sensitive = true
+resource "openstack_compute_floatingip_v2" "floatingip_1" {
+  pool = data.openstack_networking_network_v2.public.name
+}
+
+resource "openstack_compute_floatingip_associate_v2" "floatingip_1_server_1" {
+  floating_ip = openstack_compute_floatingip_v2.floatingip_1.address
+  instance_id = openstack_compute_instance_v2.server_1.id
 }
