5d8b2409e45c7b5c78d011f725a858f10c62418e
Silvio Silva <smsilva@gmail.com>
2021-07-27 19:25:10 -0300

    Novos arquivos

--- openstack/custom-image/main.tf
+++ openstack/custom-image/main.tf
@@ -15,3 +15,21 @@ resource "openstack_images_image_v2" "ubuntu" {
 resource "openstack_compute_floatingip_v2" "floatingip_1" {
   pool = data.openstack_networking_network_v2.public.name
 }
+
+data "template_file" "packer" {
+  template = file("${path.module}/packer.tpl")
+  vars = {
+    image_id             = "${openstack_images_image_v2.ubuntu.id}"
+    network_internal_id  = "${data.openstack_networking_network_v2.internal.id}"
+    ssh_keypair_name     = "${openstack_compute_keypair_v2.silvios.name}"
+    ssh_private_key_file = "/home/silvios/.ssh/id_rsa"
+    floating_ip_id       = "${openstack_compute_floatingip_v2.floatingip_1.id}"
+    floating_ip_address  = "${openstack_compute_floatingip_v2.floatingip_1.address}"
+  }
+}
+
+resource "local_file" "packer_json_file" {
+  content         = data.template_file.packer.rendered
+  filename        = "${path.module}/packer.json"
+  file_permission = "0644"
+}
