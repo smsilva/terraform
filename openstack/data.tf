f5c02c60747fb5b46e4ddfddbbfe07262172b5f1
Silvio Silva <smsilva@gmail.com>
2021-07-27 14:04:46 -0300

    Novos arquivos

--- openstack/data.tf
+++ openstack/data.tf
@@ -11,10 +11,6 @@ data "openstack_compute_flavor_v2" "m2_medium" {
 data "openstack_images_image_v2" "cirros" {
   name        = "cirros"
   most_recent = true
-
-  properties = {
-    key = "value"
-  }
 }
 
 data "openstack_networking_network_v2" "internal" {
