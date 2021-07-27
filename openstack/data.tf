c1d744590d8c220757f12570b6cc0c4fb0a2bf51
Silvio Silva <smsilva@gmail.com>
2021-07-27 14:01:20 -0300

    Novos arquivos

---
+++ openstack/data.tf
@@ -0,0 +1,26 @@
+data "openstack_compute_flavor_v2" "m1_tinny" {
+  vcpus = 1
+  ram   = 512
+}
+
+data "openstack_compute_flavor_v2" "m2_medium" {
+  vcpus = 2
+  ram   = 4096
+}
+
+data "openstack_images_image_v2" "cirros" {
+  name        = "cirros"
+  most_recent = true
+
+  properties = {
+    key = "value"
+  }
+}
+
+data "openstack_networking_network_v2" "internal" {
+  name = "test"
+}
+
+data "openstack_networking_network_v2" "public" {
+  name = "external"
+}
--- openstack/main.tf
+++ openstack/main.tf
@@ -1,30 +1,3 @@
-data "openstack_compute_flavor_v2" "m1_tinny" {
-  vcpus = 1
-  ram   = 512
-}
-
-data "openstack_compute_flavor_v2" "m2_medium" {
-  vcpus = 2
-  ram   = 4096
-}
-
-data "openstack_images_image_v2" "cirros" {
-  name        = "cirros"
-  most_recent = true
-
-  properties = {
-    key = "value"
-  }
-}
-
-data "openstack_networking_network_v2" "internal" {
-  name = "test"
-}
-
-data "openstack_networking_network_v2" "public" {
-  name = "external"
-}
-
 resource "openstack_compute_keypair_v2" "silvios" {
   name       = "silvios"
   public_key = file("/home/silvios/.ssh/id_rsa.pub")
@@ -77,6 +50,10 @@ resource "openstack_compute_floatingip_v2" "floatingip_2" {
   pool = data.openstack_networking_network_v2.public.name
 }
 
+resource "openstack_compute_floatingip_v2" "floatingip_3" {
+  pool = data.openstack_networking_network_v2.public.name
+}
+
 resource "openstack_compute_floatingip_associate_v2" "floatingip_1_server_1" {
   floating_ip = openstack_compute_floatingip_v2.floatingip_1.address
   instance_id = openstack_compute_instance_v2.server_1.id
@@ -89,27 +66,4 @@ resource "openstack_compute_floatingip_associate_v2" "floatingip_2_server_2" {
   depends_on = [
     openstack_networking_router_interface_v2.router_1_interface_1
   ]
-}
-
-resource "openstack_networking_network_v2" "network_1" {
-  name           = "network_1"
-  admin_state_up = "true"
-}
-
-resource "openstack_networking_subnet_v2" "subnet_1" {
-  name       = "subnet_1"
-  network_id = openstack_networking_network_v2.network_1.id
-  cidr       = "192.168.223.0/24"
-  ip_version = 4
-}
-
-resource "openstack_networking_router_v2" "router_1" {
-  name                = "my_router"
-  admin_state_up      = true
-  external_network_id = data.openstack_networking_network_v2.public.id
-}
-
-resource "openstack_networking_router_interface_v2" "router_1_interface_1" {
-  router_id = openstack_networking_router_v2.router_1.id
-  subnet_id = openstack_networking_subnet_v2.subnet_1.id
 }
