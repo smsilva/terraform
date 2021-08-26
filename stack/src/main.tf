f647f2fe72f977d5ce5f58dfc9414c14b7beeb4b
Silvio Silva <smsilva@gmail.com>
2021-08-26 09:11:16 -0300

    Gerando os nomes de recursos baseado no nome do ambiente

--- stack/src/main.tf
+++ stack/src/main.tf
@@ -1,18 +1,19 @@
-resource "random_string" "environment" {
+resource "random_string" "environment_id" {
   length      = 6
   special     = false
   upper       = false
   min_numeric = 4
 }
 
-resource "null_resource" "default" {
+resource "null_resource" "environment" {
   triggers = {
-    name = "${var.environment_name}-${random_string.environment.result}"
+    name                      = "${var.environment_name}-${random_string.environment_id.result}"
+    storage_account_base_name = "${var.environment_name}${random_string.environment_id.result}"
   }
 }
 
 resource "azurerm_resource_group" "default" {
-  name     = "iac-stack-${var.environment_name}"
+  name     = "iac-stack-${null_resource.environment.triggers.name}"
   location = var.region
 }
 
@@ -21,5 +22,5 @@ module "storage_account_demo" {
 
   region               = var.region
   resource_group       = azurerm_resource_group.default
-  storage_account_name = "${var.environment_name}${random_string.environment.result}demo"
+  storage_account_name = "${null_resource.environment.triggers.name}demo"
 }
--- stack/src/outputs.tf
+++ stack/src/outputs.tf
@@ -1,9 +1,9 @@
-output "input_environment_name" {
-  value = var.environment_name
+output "environment_name" {
+  value = null_resource.environment.triggers.name
 }
 
-output "environment_name" {
-  value = null_resource.default.triggers.name
+output "stack_name" {
+  value = var.stack_name
 }
 
 output "stack_version" {
